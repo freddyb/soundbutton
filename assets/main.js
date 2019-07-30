c5134d322305e92da346d45734851c223a84f350
Frederik B <freddyb@users.noreply.github.com>
2019-07-30 09:53:13 +0200

    remove inline JS, put back into main.js (part 1)
    Because inline JavaScript is bad (makes CSP adoption harder).

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,17 @@
-function playLOL(){
-  var sounds = ["assets/lol.mp3",
-               "assets/lol1.mp3",
-               "assets/lol2.mp3",
-               "assets/lol3.mp3",
-               "assets/lol4.mp3",
-               "assets/lol5.mp3",
-               "assets/lol6.mp3",
-               "assets/lol7.mp3",
-               "assets/lol8.mp3"];
-  var index = Math.floor(Math.random() * (sounds.length));
-  console.info(index);
-  let audio = new Audio(sounds[index]);
-  console.info("Button clicked");
-  audio.play();
-};
+window.addEventListener('DOMContentLoaded', function() {
+  let btn = document.getElementById("button");
+  btn.addEventListener("click", function() {
+    var sounds = ["assets/lol.mp3",
+                 "assets/lol1.mp3",
+                 "assets/lol2.mp3",
+                 "assets/lol3.mp3",
+                 "assets/lol4.mp3",
+                 "assets/lol5.mp3",
+                 "assets/lol6.mp3",
+                 "assets/lol7.mp3",
+                 "assets/lol8.mp3"];
+    var index = Math.floor(Math.random() * (sounds.length));
+    let audio = new Audio(sounds[index]);
+    audio.play();
+  });
+});
